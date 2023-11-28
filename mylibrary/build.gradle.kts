f2b691c171b2c37f5b21f40e4ba45b842cb06b36
Zoltán Bokros <zoltan.bokros@adevinta.com>
2023-11-28 18:03:46 +0100

    wip

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,6 +1,55 @@
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+/**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN**/
+val githubProperties = Properties()
+githubProperties.load(FileInputStream(rootProject.file("github.properties")))
+
+fun getVersionName(): String {
+    return "0.0.1" // Replace with version Name
+}
+
+fun getArtificatId(): String {
+    return "mylibrary" // Replace with library name ID
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            run {
+                groupId = "com.example.mylibrary"
+                artifactId = getArtificatId()
+                version = getVersionName()
+                artifact("$buildDir/outputs/aar/${getArtificatId()}-release.aar")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            /** Configure path of your package repository on Github
+             *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
+             */
+            url = uri("https://maven.pkg.github.com/zbokros/github-packages-demo")
+            credentials {
+                /**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN
+                 * OR
+                 * Set environment variables
+                 */
+                username = githubProperties["gpr.usr"] as String? ?: System.getenv("GPR_USER")
+                password =
+                    githubProperties["gpr.key"] as String? ?: System.getenv("GPR_API_KEY")
+
+            }
+        }
+    }
 }
 
 android {
@@ -24,11 +73,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
